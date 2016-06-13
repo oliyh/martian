b9986bb230d8dd23bb20a72e71ba7ccca22c5e35
Oliver Hine <git@oliy.co.uk>
2016-06-13 19:44:47 +0100

    Upgrading deps, making it jarable

--- project.clj
+++ project.clj
@@ -4,20 +4,22 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[frankiesardo/tripod "0.2.0"]
-                 [prismatic/schema "1.0.4"]
+                 [prismatic/schema "1.1.2"]
                  [camel-snake-kebab "0.4.0"]]
   :plugins [[lein-cljsbuild "1.1.1"]
             [lein-doo "0.1.6"]]
   :prep-tasks [["compile" "martian.protocols"] "javac" "compile"]
   :java-source-paths ["src"]
   :javac-options ["-target" "1.8" "-source" "1.8"]
-  :profiles {:dev {:exclusions [[org.clojure/tools.reader]]
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.8.0"]
+                                       [org.clojure/clojurescript "1.9.36"]]}
+             :dev {:exclusions [[org.clojure/tools.reader]]
                    :resource-paths ["test-resources"]
                    :dependencies [[org.clojure/clojure "1.8.0"]
-                                  [org.clojure/clojurescript "1.8.40"]
-                                  [org.clojure/tools.reader "0.10.0-alpha3"]
+                                  [org.clojure/clojurescript "1.9.36"]
+                                  [org.clojure/tools.reader "0.10.0"]
                                   [com.cemerick/piggieback "0.2.1"]
-                                  [org.mozilla/rhino "1.7.7"]]
+                                  [org.mozilla/rhino "1.7.7.1"]]
                    :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
   :aliases {"test" ["do" "clean," "test," "doo" "rhino" "test" "once"]}
   :doo {:paths {:rhino "lein run -m org.mozilla.javascript.tools.shell.Main"}}
