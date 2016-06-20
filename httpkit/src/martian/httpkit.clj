8b73e4702f604675af1e47dc60b4b542a405b203
Oliver Hine <git@oliy.co.uk>
2016-06-20 20:40:02 +0100

    Adding json deserialisation to httpkit implementation

--- httpkit/src/martian/httpkit.clj
+++ httpkit/src/martian/httpkit.clj
@@ -13,10 +13,23 @@
      :server-port (let [port (.getPort url-parsed)]
                     (when (pos? port) port))}))
 
+(defn- json? [content-type]
+  (println content-type)
+  (boolean (re-find #"application/json" content-type)))
+
+(def coerce-json
+  {:name ::coerce-json
+   :leave (fn [{:keys [response] :as ctx}]
+            (assoc ctx :response
+                   (delay (if (some-> @response
+                                      (get-in [:headers :content-type])
+                                      json?)
+                            (update @response :body json/decode keyword)
+                            @response))))})
+
 (def perform-request
   {:name ::perform-request
    :enter (fn [{:keys [request] :as ctx}]
-            (println request)
             (assoc ctx :response (http/request
                                   (-> {:as :auto}
                                       (merge request)
@@ -28,4 +41,4 @@
                                         (json/decode body keyword)))
         {:keys [scheme server-name server-port]} (parse-url url)
         base-url (format "%s://%s%s%s" (name scheme) server-name (if server-port (str ":" server-port) "") (get swagger-definition :basePath ""))]
-    (martian/bootstrap-swagger base-url swagger-definition {:interceptors [perform-request]})))
+    (martian/bootstrap-swagger base-url swagger-definition {:interceptors [coerce-json perform-request]})))
