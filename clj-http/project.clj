3fde253fa3095649bba0df49f1eeaa021ec55382
Oliver Hine <git@oliy.co.uk>
2016-06-20 20:13:27 +0100

    clj-http implementation

---
+++ clj-http/project.clj
@@ -0,0 +1,13 @@
+(defproject martian-clj-http "0.1.0-SNAPSHOT"
+  :description "clj-http implementation for martian"
+  :url "https://github.com/oliyh/martian"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[martian "0.1.0-SNAPSHOT"]
+                 [clj-http "2.2.0"]
+                 [cheshire "5.6.2"]]
+  :java-source-paths ["src"]
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :dev {:exclusions [[org.clojure/tools.reader]]
+                   :dependencies [[org.clojure/clojure "1.8.0"]
+                                  [org.clojure/tools.reader "0.10.0"]]}})
---
+++ clj-http/src/martian/clj_http.clj
@@ -0,0 +1,17 @@
+(ns martian.clj-http
+  (require [clj-http.client :as http]
+           [martian.core :as martian]))
+
+(def perform-request
+  {:name ::perform-request
+   :enter (fn [{:keys [request] :as ctx}]
+            (assoc ctx :response (http/request
+                                  (-> {:as :auto}
+                                      (merge request)
+                                      (dissoc :params)))))})
+
+(defn bootstrap-swagger [url & [params]]
+  (let [swagger-definition (:body (http/get url (merge params {:as :json})))
+        {:keys [scheme server-name server-port]} (http/parse-url url)
+        base-url (format "%s://%s%s%s" (name scheme) server-name (if server-port (str ":" server-port) "") (get swagger-definition :basePath ""))]
+    (martian/bootstrap-swagger base-url swagger-definition {:interceptors [perform-request]})))
