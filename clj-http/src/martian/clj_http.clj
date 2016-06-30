70ea0f32c0c90f2a75eddfd73a04ab2cce1f9a3f
Oliver Hine <git@oliy.co.uk>
2016-06-30 22:05:08 +0100

    Test for clj-http

--- clj-http/src/martian/clj_http.clj
+++ clj-http/src/martian/clj_http.clj
@@ -1,6 +1,15 @@
 (ns martian.clj-http
   (:require [clj-http.client :as http]
+            [cheshire.core :as json]
             [martian.core :as martian]))
+
+(def encode-body
+  {:name ::encode-body
+   :enter (fn [{:keys [request] :as ctx}]
+            (-> ctx
+                (assoc-in [:request :content-type] :json)
+                (assoc-in [:request :form-params] (:body request))
+                (update :request dissoc :body)))})
 
 (def perform-request
   {:name ::perform-request
@@ -14,4 +23,4 @@
   (let [swagger-definition (:body (http/get url (merge params {:as :json})))
         {:keys [scheme server-name server-port]} (http/parse-url url)
         base-url (format "%s://%s%s%s" (name scheme) server-name (if server-port (str ":" server-port) "") (get swagger-definition :basePath ""))]
-    (martian/bootstrap-swagger base-url swagger-definition {:interceptors [perform-request]})))
+    (martian/bootstrap-swagger base-url swagger-definition {:interceptors [encode-body perform-request]})))
---
+++ test-common/martian/server_stub.clj
@@ -0,0 +1,73 @@
+(ns martian.server-stub
+  (:require [io.pedestal.http :as bootstrap]
+            [io.pedestal.interceptor.chain :refer [terminate]]
+            [io.pedestal.interceptor :refer [interceptor]]
+            [pedestal-api
+             [core :as api]
+             [helpers :refer [before defbefore defhandler handler]]]
+            [schema.core :as s]))
+
+(defonce the-pets (atom {}))
+
+(s/defschema Pet
+  {:name s/Str
+   :type s/Str
+   :age s/Int})
+
+(defhandler create-pet
+  {:summary     "Create a pet"
+   :parameters  {:body-params Pet}
+   :responses   {201 {:body {:id s/Int}}}}
+  [request]
+  (let [id 123]
+    (swap! the-pets assoc id (:body-params request))
+    {:status 201
+     :body {:id id}}))
+
+(defhandler get-pet
+  {:summary    "Get a pet by id"
+   :parameters {:path-params {:id s/Int}}
+   :responses  {200 {:body Pet}
+                404 {:body s/Str}}}
+  [request]
+  (if-let [pet (get @the-pets (get-in request [:path-params :id]))]
+    {:status 200
+     :body pet}
+    {:status 404
+     :body "No pet found with this id"}))
+
+(s/with-fn-validation
+  (api/defroutes routes
+    {}
+    [[["/" ^:interceptors [api/error-responses
+                           (api/negotiate-response)
+                           (api/body-params)
+                           api/common-body
+                           (api/coerce-request)
+                           (api/validate-response)]
+       ["/pets"
+        ["/" {:post create-pet}]
+        ["/:id" {:get get-pet}]]
+
+       ["/swagger.json" {:get api/swagger-json}]]]]))
+
+(def service
+  {:env                      :dev
+   ::bootstrap/routes        #(deref #'routes)
+   ;; linear-search, and declaring the swagger-ui handler last in the routes,
+   ;; is important to avoid the splat param for the UI matching API routes
+   ::bootstrap/router        :linear-search
+   ::bootstrap/resource-path "/public"
+   ::bootstrap/type          :jetty
+   ::bootstrap/port          8888
+   ::bootstrap/join?         false})
+
+(def swagger-url (format "http://localhost:%s/swagger.json" (::bootstrap/port service)))
+
+(def with-server
+  (fn [f]
+    (let [server-instance (bootstrap/create-server (bootstrap/default-interceptors service))]
+      (try
+        (bootstrap/start server-instance)
+        (f)
+        (finally (bootstrap/stop server-instance))))))
