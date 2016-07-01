7a6c40684290a2d47d05d26b752d4df883da7f82
Oliver Hine <git@oliy.co.uk>
2016-07-01 23:22:54 +0100

    Supporting various content types in clj-http

--- clj-http/src/martian/clj_http.clj
+++ clj-http/src/martian/clj_http.clj
@@ -1,26 +1,66 @@
 (ns martian.clj-http
   (:require [clj-http.client :as http]
             [cheshire.core :as json]
-            [martian.core :as martian]))
+            [cognitect.transit :as transit]
+            [martian.core :as martian])
+  (:import [java.io ByteArrayInputStream]))
+
+(def ^:private supported-encodings
+  ["application/transit+msgpack"
+   "application/transit+json"
+   "application/edn"
+   "application/json"])
+
+(defn- choose-content-type [options]
+  (some (set options) supported-encodings))
 
 (def encode-body
   {:name ::encode-body
-   :enter (fn [{:keys [request] :as ctx}]
-            (-> ctx
-                (assoc-in [:request :content-type] :json)
-                (assoc-in [:request :form-params] (:body request))
-                (update :request dissoc :body)))})
+   :enter (fn [{:keys [request handler] :as ctx}]
+            (if-let [content-type (and (:body request)
+                                       (not (get-in request [:headers "Content-Type"]))
+                                       (choose-content-type (get-in handler [:swagger-definition :consumes])))]
+              (-> ctx
+                  (update-in [:request :body]
+                             (condp = content-type
+                               "application/json" json/encode
+                               "application/edn" pr-str
+                               "application/transit+json" #(http/transit-encode % :json)
+                               "application/transit+msgpack" #(http/transit-encode % :msgpack)
+                               identity))
+                  (assoc-in [:request :headers "Content-Type"] content-type))
+              ctx))})
+
+(def coerce-response
+  {:name ::coerce-response
+   :enter (fn [{:keys [request handler] :as ctx}]
+            (if-let [content-type (and (not (get-in request [:headers "Accept"]))
+                                       (choose-content-type (get-in handler [:swagger-definition :produces])))]
+              (cond-> (assoc-in ctx [:request :headers "Accept"] content-type)
+                (= "application/transit+msgpack" content-type) (assoc-in [:request :as] :byte-array))
+
+              (assoc-in ctx [:request :as] :auto)))
+
+   :leave (fn [{:keys [request response handler] :as ctx}]
+            (if-let [content-type (and (:body response)
+                                       (not= :auto (:as request))
+                                       (not-empty (get-in response [:headers "Content-Type"])))]
+              (update-in ctx [:response :body]
+                         (condp re-find content-type
+                           #"application/json" #(json/decode % keyword)
+                           #"application/edn" http/parse-edn
+                           #"application/transit\+json" #(http/parse-transit (ByteArrayInputStream. (.getBytes %)) :json)
+                           #"application/transit\+msgpack" #(http/parse-transit (ByteArrayInputStream. %) :msgpack)
+                           identity))
+              ctx))})
 
 (def perform-request
   {:name ::perform-request
    :enter (fn [{:keys [request] :as ctx}]
-            (assoc ctx :response (http/request
-                                  (-> {:as :auto}
-                                      (merge request)
-                                      (dissoc :params)))))})
+            (assoc ctx :response (http/request (dissoc request :params))))})
 
 (defn bootstrap-swagger [url & [params]]
   (let [swagger-definition (:body (http/get url (merge params {:as :json})))
         {:keys [scheme server-name server-port]} (http/parse-url url)
         base-url (format "%s://%s%s%s" (name scheme) server-name (if server-port (str ":" server-port) "") (get swagger-definition :basePath ""))]
-    (martian/bootstrap-swagger base-url swagger-definition {:interceptors [encode-body perform-request]})))
+    (martian/bootstrap-swagger base-url swagger-definition {:interceptors [encode-body coerce-response perform-request]})))
