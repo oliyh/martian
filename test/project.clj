a540babd93597c54451b4b9effb34d0b4f0d80be
Oliver Hine <git@oliy.co.uk>
2016-07-01 21:57:04 +0100

    Switching test module tests to nashorn

--- test/project.clj
+++ test/project.clj
@@ -8,7 +8,6 @@
             [lein-doo "0.1.6"]]
   :dependencies [[martian :version]
                  [prismatic/schema-generators "0.1.0"]]
-  :java-source-paths ["src"]
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.8.0"]
                                        [org.clojure/clojurescript "1.9.36"]]}
              :dev {:exclusions [[org.clojure/tools.reader]]
@@ -17,11 +16,9 @@
                                   [org.clojure/clojurescript "1.9.36"]
                                   [prismatic/schema "1.1.2"]
                                   [org.clojure/tools.reader "0.10.0"]
-                                  [com.cemerick/piggieback "0.2.1"]
-                                  [org.mozilla/rhino "1.7.7.1"]]
+                                  [com.cemerick/piggieback "0.2.1"]]
                    :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
-  :aliases {"test" ["do" ["clean"] ["test"] ["doo" "rhino" "test" "once"]]}
-  :doo {:paths {:rhino "lein run -m org.mozilla.javascript.tools.shell.Main"}}
+  :aliases {"test" ["do" ["clean"] ["test"] ["doo" "nashorn" "test" "once"]]}
   :cljsbuild {:builds [{:id "test"
                         :source-paths ["src" "test"]
                         :compiler {:output-to "target/unit-test.js"
