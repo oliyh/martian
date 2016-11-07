c4d4ac9172a2a9df7bc6751104ff63ada4f6a68d
Oliver Hine <git@oliy.co.uk>
2016-11-07 18:23:00 +0000

    Version 0.1.1

--- httpkit/project.clj
+++ httpkit/project.clj
@@ -1,4 +1,4 @@
-(defproject martian-httpkit "0.1.1-SNAPSHOT"
+(defproject martian-httpkit "0.1.1"
   :description "httpkit implementation for martian"
   :url "https://github.com/oliyh/martian"
   :license {:name "The MIT License"
