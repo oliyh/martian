ee1b4643d7ba0a114a96b9214934421aeff5984f
Oliver Hine <git@oliy.co.uk>
2018-02-14 09:31:56 +0000

    Making encoding/decoding more pluggable, starts to address #23

--- httpkit/src/martian/httpkit.clj
+++ httpkit/src/martian/httpkit.clj
@@ -5,9 +5,12 @@
             [cognitect.transit :as transit]
             [clojure.edn :as edn]
             [clojure.java.io :as io]
-            [tripod.context :as tc])
+            [tripod.context :as tc]
+            [clojure.string :as string]
+            [linked.core :as linked])
   (:import [java.net URL]
-           [java.io ByteArrayInputStream ByteArrayOutputStream]))
+           [java.io ByteArrayInputStream ByteArrayOutputStream]
+           [java.util.regex Pattern]))
 
 (defn- parse-url
   "Parse a URL string into a map of interesting parts. Lifted from clj-http."
@@ -27,57 +30,83 @@
     (transit/write writer body)
     (io/input-stream (.toByteArray out))))
 
-(def supported-encodings
-  ["application/transit+msgpack"
-   "application/transit+json"
-   "application/edn"
-   "application/json"])
+(defn- choose-content-type [encoders options]
+  (some (set options) (keys encoders)))
 
-(defn- choose-content-type [options]
-  (some (set options) supported-encodings))
+(def auto-encoder
+  {:encode identity
+   :decode identity
+   :as :auto})
 
-(def encode-body
-  {:name ::encode-body
-   :enter (fn [{:keys [request handler] :as ctx}]
-            (if-let [content-type (and (:body request)
+(defn default-encoders
+  ([] (default-encoders keyword))
+  ([key-fn]
+   (linked/map
+    "application/transit+msgpack" {:encode #(transit-encode % :msgpack)
+                                   :decode #(transit-decode % :msgpack)
+                                   :as :byte-array}
+    "application/transit+json"    {:encode #(transit-encode % :json)
+                                   :decode #(transit-decode (.getBytes ^String %) :json)}
+    "application/edn"             {:encode pr-str
+                                   :decode edn/read-string}
+    "application/json"            {:encode json/encode
+                                   :decode #(json/decode % key-fn)})))
+
+(defn- compile-encoder-matches [encoders]
+  (reduce-kv (fn [acc content-type encoder]
+               (assoc-in acc [content-type :matcher] (re-pattern (Pattern/quote content-type))))
+             encoders
+             encoders))
+
+(defn- find-encoder [encoders content-type]
+  (if (string/blank? content-type)
+    auto-encoder
+    (loop [encoders (vals encoders)]
+      (let [{:keys [matcher] :as encoder} (first encoders)]
+        (cond
+          (not encoder) auto-encoder
+
+          (re-find matcher content-type) encoder
+
+          :else
+          (recur (rest encoders)))))))
+
+(defn encode-body
+  ([] (encode-body (default-encoders)))
+  ([encoders]
+   (let [encoders (compile-encoder-matches encoders)]
+     {:name ::encode-body
+      :enter (fn [{:keys [request handler] :as ctx}]
+               (let [content-type (and (:body request)
                                        (not (get-in request [:headers "Content-Type"]))
-                                       (choose-content-type (:consumes handler)))]
-              (-> ctx
-                  (update-in [:request :body]
-                             (condp = content-type
-                               "application/json" json/encode
-                               "application/edn" pr-str
-                               "application/transit+json" #(transit-encode % :json)
-                               "application/transit+msgpack" #(transit-encode % :msgpack)
-                               identity))
-                  (assoc-in [:request :headers "Content-Type"] content-type))
-              ctx))})
+                                       (choose-content-type encoders (:consumes handler)))
+                     {:keys [encode] :as encoder} (find-encoder encoders content-type)]
+                 (cond-> ctx
+                     (get-in ctx [:request :body]) (update-in [:request :body] encode)
+                     content-type (assoc-in [:request :headers "Content-Type"] content-type))))})))
+
+(def default-encode-body (encode-body))
 
 (defn coerce-response
-  ([] (coerce-response {}))
-  ([{:keys [key-fn] :or {key-fn keyword}}]
-   {:name ::coerce-response
-    :enter (fn [{:keys [request handler] :as ctx}]
-             (if-let [content-type (and (not (get-in request [:headers "Accept"]))
-                                        (choose-content-type (:produces handler)))]
-               (cond-> (assoc-in ctx [:request :headers "Accept"] content-type)
-                 :always (assoc-in [:request :as] :text)
-                 (= "application/transit+msgpack" content-type) (assoc-in [:request :as] :byte-array))
+  ([] (coerce-response (default-encoders)))
+  ([encoders]
+   (let [encoders (compile-encoder-matches encoders)]
+     {:name ::coerce-response
+      :enter (fn [{:keys [request handler] :as ctx}]
+               (let [content-type (and (not (get-in request [:headers "Accept"]))
+                                       (choose-content-type encoders (:produces handler)))
+                     {:keys [as] :or {as :text}} (find-encoder encoders content-type)]
 
-               (assoc-in ctx [:request :as] :auto)))
+                 (-> ctx
+                     (assoc-in [:request :headers "Accept"] content-type)
+                     (assoc-in [:request :as] as))))
 
-    :leave (fn [{:keys [request response handler] :as ctx}]
-             (assoc ctx :response
-                    (if-let [content-type (and (:body response)
-                                               (not-empty (get-in response [:headers :content-type])))]
-                      (update response :body
-                              (condp re-find content-type
-                                #"application/json" #(json/decode % key-fn)
-                                #"application/edn" edn/read-string
-                                #"application/transit\+json" #(transit-decode (.getBytes ^String %) :json)
-                                #"application/transit\+msgpack" #(transit-decode % :msgpack)
-                                identity))
-                      response)))}))
+      :leave (fn [{:keys [request response handler] :as ctx}]
+               (assoc ctx :response
+                      (let [content-type (and (:body response)
+                                              (not-empty (get-in response [:headers :content-type])))
+                            {:keys [matcher decode] :as encoder} (find-encoder encoders content-type)]
+                        (update response :body decode))))})))
 
 (def default-coerce-response (coerce-response))
 
@@ -95,7 +124,7 @@
                                        (:response (tc/execute (assoc ctx :response response))))))))})
 
 (def default-interceptors
-  (concat martian/default-interceptors [encode-body default-coerce-response perform-request]))
+  (concat martian/default-interceptors [default-encode-body default-coerce-response perform-request]))
 
 (defn bootstrap [api-root concise-handlers & [opts]]
   (martian/bootstrap api-root concise-handlers (merge {:interceptors default-interceptors} opts)))
--- httpkit/test/martian/httpkit_test.clj
+++ httpkit/test/martian/httpkit_test.clj
@@ -2,12 +2,30 @@
   (:require [martian.httpkit :as martian-http]
             [martian.core :as martian]
             [martian.server-stub :refer [with-server swagger-url]]
-            [clojure.test :refer :all]))
+            [clojure.test :refer :all]
+            [clojure.java.io :as io])
+  (:import [java.io ByteArrayOutputStream]))
 
 (use-fixtures :once with-server)
 
+(defn- input-stream->byte-array [input-stream]
+  (with-open [os (ByteArrayOutputStream.)]
+    (io/copy (io/input-stream input-stream) os)
+    (.toByteArray os)))
+
 (deftest http-test
   (let [m (martian-http/bootstrap-swagger swagger-url)]
+
+    (is (= {:method :post
+            :url "http://localhost:8888/pets/"
+            :body {:name "Doggy McDogFace", :type "Dog", :age 3}
+            :headers {"Accept" "application/transit+msgpack"
+                      "Content-Type" "application/transit+msgpack"}
+            :as :byte-array}
+           (-> (martian/request-for m :create-pet {:pet {:name "Doggy McDogFace"
+                                                         :type "Dog"
+                                                         :age 3}})
+               (update :body #(martian-http/transit-decode (input-stream->byte-array %) :msgpack)))))
 
     (let [response @(martian/response-for m :create-pet {:pet {:name "Doggy McDogFace"
                                                                :type "Dog"
