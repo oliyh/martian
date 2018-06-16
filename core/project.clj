5712fb31450e9f2cbd1bdbb1839ffa9c1b9d3a44
Oliver Hine <git@oliy.co.uk>
2018-06-16 23:03:22 +0100

    repl working again on jdk 10

--- core/project.clj
+++ core/project.clj
@@ -5,14 +5,27 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[frankiesardo/tripod "0.2.0"]
                  [prismatic/schema "1.1.9"]
+                 [metosin/spec-tools "0.7.0"
+                  :exclusions [com.fasterxml.jackson.core/jackson-databind]]
+                 [org.clojure/spec.alpha "0.1.143"]
                  [camel-snake-kebab "0.4.0"]
-                 [cheshire "5.8.0"]
+
+                 [cheshire "5.8.0"
+                  :exclusions [com.fasterxml.jackson.core/jackson-core
+                               com.fasterxml.jackson.dataformat/jackson-dataformat-cbor
+                               com.fasterxml.jackson.dataformat/jackson-dataformat-smile]]
+                 [com.fasterxml.jackson.core/jackson-core "2.9.5"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.9.5"]
+                 [com.fasterxml.jackson.dataformat/jackson-dataformat-cbor "2.9.5"]
+                 [com.fasterxml.jackson.dataformat/jackson-dataformat-smile "2.9.5"]
+
                  [com.cognitect/transit-clj "0.8.300"]
                  [com.cognitect/transit-cljs "0.8.243"]
                  [frankiesardo/linked "1.2.9"]]
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-doo "0.1.8"]]
   :java-source-paths ["src"]
+  :jvm-opts ["--add-modules" "java.xml.bind"]
   :javac-options ["-target" "1.8" "-source" "1.8"]
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.9.0"]
                                        [org.clojure/clojurescript "1.9.946"]]}
@@ -22,8 +35,9 @@
                    :dependencies [[org.clojure/clojure "1.9.0"]
                                   [org.clojure/clojurescript "1.9.946"]
                                   [org.clojure/tools.reader "1.2.2"]
-                                  [com.cemerick/piggieback "0.2.2"]]
-                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
+                                  [cider/piggieback "0.3.6"]
+                                  [org.clojure/tools.nrepl "0.2.13"]]
+                   :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}}}
   :aliases {"test" ["do" ["clean"] ["test"] ["doo" "nashorn" "test" "once"]]}
   :cljsbuild {:builds [{:id "test"
                         :source-paths ["src" "test"]
