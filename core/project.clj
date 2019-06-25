db567e96d907d479cfc9e21518d4efa0b9cd1248
Oliver Hine <git@oliy.co.uk>
2019-06-25 20:12:06 +0100

    Version 0.1.10

--- core/project.clj
+++ core/project.clj
@@ -1,4 +1,4 @@
-(defproject martian "0.1.10-SNAPSHOT"
+(defproject martian "0.1.10"
   :description "Client routing for Swagger APIs"
   :url "https://github.com/oliyh/martian"
   :license {:name "The MIT License"
