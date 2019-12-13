3255bed76e8c90a9e1168cb32823be439bcd32fd
Oliver Hine <git@oliy.co.uk>
2019-12-13 14:36:43 -0800

    Version 0.1.11

--- cljs-http/project.clj
+++ cljs-http/project.clj
@@ -1,4 +1,4 @@
-(defproject martian-cljs-http "0.1.11-SNAPSHOT"
+(defproject martian-cljs-http "0.1.11"
   :description "cljs-http implementation for martian"
   :url "https://github.com/oliyh/martian"
   :license {:name "The MIT License"
