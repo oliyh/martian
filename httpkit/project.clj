d21f52076b84310cda6a151dd054a699495a66a2
Rafael Leal <rafaelealdias@gmail.com>
2019-12-19 13:22:37 -0300

    Bump every module

--- httpkit/project.clj
+++ httpkit/project.clj
@@ -1,4 +1,4 @@
-(defproject martian-httpkit "0.1.12-SNAPSHOT"
+(defproject martian-httpkit "0.1.13-SNAPSHOT"
   :description "httpkit implementation for martian"
   :url "https://github.com/oliyh/martian"
   :license {:name "The MIT License"
--- re-frame/project.clj
+++ re-frame/project.clj
@@ -1,4 +1,4 @@
-(defproject martian-re-frame "0.1.12-SNAPSHOT"
+(defproject martian-re-frame "0.1.13-SNAPSHOT"
   :description "re-frame bindings for martian"
   :url "https://github.com/oliyh/martian"
   :license {:name "The MIT License"
