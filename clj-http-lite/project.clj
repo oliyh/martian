044bb7a96e307331f8dda3cd65232847ebafabb2
Oliver Hine <git@oliy.co.uk>
2020-04-28 21:10:08 +0100

    Version 0.1.12

--- clj-http-lite/project.clj
+++ clj-http-lite/project.clj
@@ -1,4 +1,4 @@
-(defproject martian-clj-http-lite "0.1.12-SNAPSHOT"
+(defproject martian-clj-http-lite "0.1.12"
   :description "clj-http-lite implementation for martian"
   :url "https://github.com/oliyh/martian"
   :license {:name "The MIT License"
--- test/project.clj
+++ test/project.clj
@@ -1,4 +1,4 @@
-(defproject martian-test "0.1.12-SNAPSHOT"
+(defproject martian-test "0.1.12"
   :description "Testing tools for martian"
   :url "https://github.com/oliyh/martian"
   :license {:name "The MIT License"
