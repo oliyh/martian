08fee7fcf12236a811598a9b2012d61a2956cb4f
Oliver Hine <git@oliy.co.uk>
2020-06-08 20:39:56 +0100

    First go

---
+++ vcr/README.md
@@ -0,0 +1,4 @@
+# martian-vcr
+`martian-vcr` is a library which allows you to easily record and play back HTTP requests made through martian.
+
+## Usage
---
+++ vcr/project.clj
@@ -0,0 +1,14 @@
+(defproject martian-vcr "0.1.13-SNAPSHOT"
+  :description "Recording and playback for Martian"
+  :url "https://github.com/oliyh/martian"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :plugins [[lein-modules "0.3.11"]]
+  :dependencies [[martian :version]]
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.10.1"]]}
+             :dev {:source-paths ["../test-common"]
+                   :exclusions [[org.clojure/tools.reader]]
+                   :dependencies [[org.clojure/tools.reader "1.2.2"]
+                                  [pedestal-api "0.3.4"]
+                                  [io.pedestal/pedestal.service "0.5.3"]
+                                  [io.pedestal/pedestal.jetty "0.5.3"]]}})
