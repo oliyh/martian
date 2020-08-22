0b15e1d866761b2e8018cb8f64a225c916534fbd
Oliver Hine <git@oliy.co.uk>
2020-08-22 20:53:55 +0100

    Version 0.1.13

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject martian-suite "0.1.13-SNAPSHOT"
+(defproject martian-suite "0.1.13"
   :description "Client routing for Swagger APIs"
   :url "https://github.com/oliyh/martian"
   :license {:name "The MIT License"
