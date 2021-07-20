16483ef525aa0918ff1bf7be3bdfdc7d915a5b8b
Oliver Hine <git@oliy.co.uk>
2021-07-20 14:21:38 +0100

    Version 0.1.18-SNAPSHOT

--- vcr/project.clj
+++ vcr/project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.oliyh/martian-vcr "0.1.17"
+(defproject com.github.oliyh/martian-vcr "0.1.18-SNAPSHOT"
   :description "Recording and playback for Martian"
   :url "https://github.com/oliyh/martian"
   :license {:name "The MIT License"
