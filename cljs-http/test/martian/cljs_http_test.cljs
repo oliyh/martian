7d23b1a60e6d6f6f965533b655b56adefa8ca2a5
Dorab Patel <dorabpatel@gmail.com>
2022-01-17 15:46:33 -0800

    Added tests for OpenAPI absolute server URL support.
    Used local defs for testing URLs.

--- cljs-http/test/martian/cljs_http_test.cljs
+++ cljs-http/test/martian/cljs_http_test.cljs
@@ -5,9 +5,14 @@
             [cljs.core.async :refer [<! timeout]])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
+(def swagger-url "http://localhost:8888/swagger.json")
+(def openapi-url "http://localhost:8888/openapi.json")
+(def openapi-test-url "http://localhost:8888/openapi-test.json")
+
+
 (deftest swagger-http-test
   (async done
-         (go (let [m (<! (martian-http/bootstrap-swagger "http://localhost:8888/swagger.json"))]
+         (go (let [m (<! (martian-http/bootstrap-swagger swagger-url))]
 
                (let [response (<! (martian/response-for m :create-pet {:pet {:name "Doggy McDogFace"
                                                                              :type "Dog"
@@ -25,7 +30,19 @@
 
 (deftest openapi-bootstrap-test
   (async done
-         (go (let [m (<! (martian-http/bootstrap-openapi "http://localhost:8888/openapi.json"))]
+         (go (let [m (<! (martian-http/bootstrap-openapi openapi-url))
+                   mt (<! (martian-http/bootstrap-openapi openapi-test-url))
+                   mt1 (<! (martian-http/bootstrap-openapi openapi-test-url {:server-url "https://sandbox.com"}))
+                   mt2 (<! (martian-http/bootstrap-openapi openapi-test-url {:server-url "/v3.1"}))]
+
+               (is (= "https://sandbox.example.com"
+                      (:api-root mt)) "check absolute server url")
+
+               (is (= "https://sandbox.com"
+                      (:api-root mt1)) "check absolute server url via opts")
+
+               (is (= "http://localhost:8888/v3.1"
+                      (:api-root mt2)) "check relative server url via opts")
 
                (is (= "http://localhost:8888/openapi/v3"
                       (:api-root m)))
