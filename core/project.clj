f1ba5a7d4e8a0f43f2b56f32f8265eedb42b83ec
Oliver Hine <git@oliy.co.uk>
2022-02-25 15:37:06 +0000

    Version 0.1.21

--- core/project.clj
+++ core/project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.oliyh/martian "0.1.21-SNAPSHOT"
+(defproject com.github.oliyh/martian "0.1.21"
   :description "Client routing for Swagger APIs"
   :url "https://github.com/oliyh/martian"
   :license {:name "The MIT License"
