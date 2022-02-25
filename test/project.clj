fee7421aad24cdc622b9083fbe583abcff31cdd6
Oliver Hine <git@oliy.co.uk>
2022-02-25 15:40:06 +0000

    Version 0.1.22-SNAPSHOT

--- test/project.clj
+++ test/project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.oliyh/martian-test "0.1.21"
+(defproject com.github.oliyh/martian-test "0.1.22-SNAPSHOT"
   :description "Testing tools for martian"
   :url "https://github.com/oliyh/martian"
   :license {:name "The MIT License"
