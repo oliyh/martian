f84416f0adf5cede2ce40815b8d7f4de4823fe03
Oliver Hine <git@oliy.co.uk>
2022-11-28 16:07:51 +0000

    Version 0.1.23-SNAPSHOT

--- core/project.clj
+++ core/project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.oliyh/martian "0.1.22"
+(defproject com.github.oliyh/martian "0.1.23-SNAPSHOT"
   :description "Client routing for Swagger APIs"
   :url "https://github.com/oliyh/martian"
   :license {:name "The MIT License"
--- httpkit/project.clj
+++ httpkit/project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.oliyh/martian-httpkit "0.1.22"
+(defproject com.github.oliyh/martian-httpkit "0.1.23-SNAPSHOT"
   :description "httpkit implementation for martian"
   :url "https://github.com/oliyh/martian"
   :license {:name "The MIT License"
