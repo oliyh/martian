9436ccc24cdf2d699786d0f436d19ee408e7e261
Michiel Borkent <michielborkent@gmail.com>
2023-03-13 11:35:28 +0100

    Add babashka-http-client module

---
+++ babashka-http-client/project.clj
@@ -0,0 +1,16 @@
+(defproject com.github.oliyh/martian-babashka-http-client "0.1.23-SNAPSHOT"
+  :description "babashka http-client implementation for martian"
+  :url "https://github.com/oliyh/martian"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :plugins [[lein-modules "0.3.11"]]
+  :dependencies [[com.github.oliyh/martian :version]
+                 [org.babashka/json "0.1.1"]
+                 [org.babashka/http-client "0.1.6"]]
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.11.1"]]}
+             :dev {:source-paths ["../test-common"]
+                   :exclusions [[org.clojure/tools.reader]]
+                   :dependencies [[org.clojure/tools.reader "1.3.5"]
+                                  [pedestal-api "0.3.5"]
+                                  [io.pedestal/pedestal.service "0.5.9"]
+                                  [io.pedestal/pedestal.jetty "0.5.9"]]}})
---
+++ babashka-http-client/test/martian/babashka/http_client_test.clj
@@ -0,0 +1,124 @@
+(ns martian.babashka.http-client-test
+  (:require [martian.babashka.http-client :as martian-http]
+            [martian.core :as martian]
+            [martian.encoders :as encoders]
+            [martian.server-stub :refer [with-server swagger-url openapi-url openapi-test-url openapi-yaml-url openapi-test-yaml-url]]
+            [martian.test-utils :refer [input-stream->byte-array]]
+            [clojure.test :refer [deftest testing is use-fixtures]]))
+
+(use-fixtures :once with-server)
+
+(deftest swagger-http-test
+  (let [m (martian-http/bootstrap-swagger swagger-url)]
+
+    (testing "default encoders"
+      (is (= {:method :post
+              :url "http://localhost:8888/pets/"
+              :body {:name "Doggy McDogFace", :type "Dog", :age 3}
+              :headers {"Accept" "application/transit+msgpack"
+                        "Content-Type" "application/transit+msgpack"}
+              :as :byte-array
+              :version :http-1.1}
+             (-> (martian/request-for m :create-pet {:pet {:name "Doggy McDogFace"
+                                                           :type "Dog"
+                                                           :age 3}})
+                 (update :body #(encoders/transit-decode (input-stream->byte-array %) :msgpack))))))
+
+
+    (let [response (martian/response-for m :create-pet {:pet {:name "Doggy McDogFace"
+                                                              :type "Dog"
+                                                              :age 3}})]
+      (is (= {:status 201
+              :body {:id 123}}
+             (select-keys response [:status :body]))))
+
+    (let [response (martian/response-for m :get-pet {:id 123})]
+      (is (= {:name "Doggy McDogFace"
+              :type "Dog"
+              :age 3}
+             (:body response))))))
+
+(deftest async-test
+  (let [m (martian-http/bootstrap-swagger swagger-url {:interceptors martian-http/default-interceptors-async})]
+
+    (testing "default encoders"
+      (is (= {:method :post
+              :url "http://localhost:8888/pets/"
+              :body {:name "Doggy McDogFace", :type "Dog", :age 3}
+              :headers {"Accept" "application/transit+msgpack"
+                        "Content-Type" "application/transit+msgpack"}
+              :as :byte-array
+              :version :http-1.1}
+             (-> (martian/request-for m :create-pet {:pet {:name "Doggy McDogFace"
+                                                           :type "Dog"
+                                                           :age 3}})
+                 (update :body #(encoders/transit-decode (input-stream->byte-array %) :msgpack))))))
+
+
+    (let [response @(martian/response-for m :create-pet {:pet {:name "Doggy McDogFace"
+                                                               :type "Dog"
+                                                               :age 3}})]
+      (is (= {:status 201
+              :body {:id 123}}
+             (select-keys response [:status :body]))))
+
+    (let [response @(martian/response-for m :get-pet {:id 123})]
+      (is (= {:name "Doggy McDogFace"
+              :type "Dog"
+              :age 3}
+             (:body response))))))
+
+(deftest error-handling-test
+  (testing "remote exceptions"
+
+    (testing "can be thrown"
+      (let [m (martian-http/bootstrap-swagger swagger-url {:interceptors martian-http/default-interceptors-async})]
+        (is (thrown? Exception @(martian/response-for m :get-pet {:id -1})))))
+
+    (testing "can be handled"
+      (let [turn-off-exception-throwing {:name ::turn-off-exception-throwing
+                                         :enter (fn [ctx] (assoc-in ctx [:request :throw] false))}
+            m (martian-http/bootstrap-swagger swagger-url {:interceptors (cons turn-off-exception-throwing
+                                                                               martian-http/default-interceptors-async)})]
+        (is (= 404 (:status @(martian/response-for m :get-pet {:id -1}))))))
+
+    (testing "can be caught in the interceptor chain"
+      (let [m (martian-http/bootstrap-swagger swagger-url {:interceptors (cons {:name ::i-catch-errors
+                                                                                :error (fn [ctx _error]
+                                                                                         (assoc ctx :response {:custom-error-response true}))}
+                                                                               martian-http/default-interceptors-async)})]
+        (is (= {:custom-error-response true}
+               @(martian/response-for m :get-pet {:id -1})))))))
+
+(deftest openapi-bootstrap-test
+  (let [m (martian-http/bootstrap-openapi openapi-url)
+        myaml (martian-http/bootstrap-openapi openapi-yaml-url)]
+
+    (is (= "https://sandbox.example.com"
+           (:api-root (martian-http/bootstrap-openapi openapi-test-yaml-url)))
+        "check yaml description")
+
+    (is (= "https://sandbox.example.com"
+           (:api-root (martian-http/bootstrap-openapi openapi-test-url)))
+        "check absolute server url")
+
+    (is (= "https://sandbox.com"
+           (:api-root (martian-http/bootstrap-openapi openapi-test-url {:server-url "https://sandbox.com"})))
+        "check absolute server url via opts")
+
+    (is (= "http://localhost:8888/v3.1"
+           (:api-root (martian-http/bootstrap-openapi openapi-test-url {:server-url "/v3.1"})))
+        "check relative server url via opts")
+
+    (is (= "http://localhost:8888/openapi/v3"
+           (:api-root myaml))
+        "check yaml description")
+
+    (is (contains? (set (map first (martian/explore myaml)))
+                   :get-order-by-id))
+
+    (is (= "http://localhost:8888/openapi/v3"
+           (:api-root m)))
+
+    (is (contains? (set (map first (martian/explore m)))
+                   :get-order-by-id))))
