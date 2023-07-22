8347be3af8be6b0ea2e0e57c7a84318953313c96
Oliver Hine <git@oliy.co.uk>
2023-07-22 15:50:39 +0100

    Version 0.1.26-SNAPSHOT

--- hato/project.clj
+++ hato/project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.oliyh/martian-hato "0.1.25"
+(defproject com.github.oliyh/martian-hato "0.1.26-SNAPSHOT"
   :description "hato implementation for martian"
   :url "https://github.com/oliyh/martian"
   :license {:name "The MIT License"
