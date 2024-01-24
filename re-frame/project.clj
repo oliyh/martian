eebf3da77789b45082d58d42560da268c0dc0b6a
Oliver Hine <git@oliy.co.uk>
2024-01-24 09:41:40 +0000

    Version 0.1.27-SNAPSHOT

--- re-frame/project.clj
+++ re-frame/project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.oliyh/martian-re-frame "0.1.26"
+(defproject com.github.oliyh/martian-re-frame "0.1.27-SNAPSHOT"
   :description "re-frame bindings for martian"
   :url "https://github.com/oliyh/martian"
   :license {:name "The MIT License"
