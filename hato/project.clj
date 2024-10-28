4fa5ae65e780c739629600fa3aab08e8771351c1
Oliver Hine <git@oliy.co.uk>
2024-10-28 20:52:37 +0000

    Version 0.1.29-SNAPSHOT

--- hato/project.clj
+++ hato/project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.oliyh/martian-hato "0.1.28"
+(defproject com.github.oliyh/martian-hato "0.1.29-SNAPSHOT"
   :description "hato implementation for martian"
   :url "https://github.com/oliyh/martian"
   :license {:name "The MIT License"
--- vcr/project.clj
+++ vcr/project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.oliyh/martian-vcr "0.1.28"
+(defproject com.github.oliyh/martian-vcr "0.1.29-SNAPSHOT"
   :description "Recording and playback for Martian"
   :url "https://github.com/oliyh/martian"
   :license {:name "The MIT License"
