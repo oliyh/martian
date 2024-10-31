0fc7de6d5efc2851047f46c676c4004ba7aff767
Oliver Hine <git@oliy.co.uk>
2024-10-31 10:15:34 +0000

    Adding ring codec as a top level dep

--- core/project.clj
+++ core/project.clj
@@ -16,7 +16,8 @@
 
                  [com.cognitect/transit-clj "1.0.324"]
                  [com.cognitect/transit-cljs "0.8.269"]
-                 [org.flatland/ordered "1.15.10"]]
+                 [org.flatland/ordered "1.15.10"]
+                 [ring/ring-codec "1.2.0"]]
   :java-source-paths ["src"]
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.10.3"]
                                        [org.clojure/clojurescript "1.10.866"]]}
