475ad26d014fbbf30abe1adfa62caeabfe85c558
Oliver Hine <git@oliy.co.uk>
2025-06-10 17:06:53 +0100

    Version 0.1.33-SNAPSHOT

--- cljs-http/project.clj
+++ cljs-http/project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.oliyh/martian-cljs-http "0.1.32"
+(defproject com.github.oliyh/martian-cljs-http "0.1.33-SNAPSHOT"
   :description "cljs-http implementation for martian"
   :url "https://github.com/oliyh/martian"
   :license {:name "The MIT License"
