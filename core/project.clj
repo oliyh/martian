bbab42ac186572cdd5a6dd2b668e8ea29c3e1020
Oliver Hine <git@oliy.co.uk>
2025-07-25 15:42:40 +0100

    Version 0.2.1-SNAPSHOT

--- core/project.clj
+++ core/project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.oliyh/martian "0.2.0"
+(defproject com.github.oliyh/martian "0.2.1-SNAPSHOT"
   :description "Client routing for Swagger APIs"
   :url "https://github.com/oliyh/martian"
   :license {:name "The MIT License"
--- vcr/project.clj
+++ vcr/project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.oliyh/martian-vcr "0.2.0"
+(defproject com.github.oliyh/martian-vcr "0.2.1-SNAPSHOT"
   :description "Recording and playback for Martian"
   :url "https://github.com/oliyh/martian"
   :license {:name "The MIT License"
